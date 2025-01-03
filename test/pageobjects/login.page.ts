3c91e86966680765c23201f7085fd4edce539138
haite4 <ugrayvcsgo137@gmail.com>
2025-01-03 12:32:05 +0000

    Initial commit

---
+++ test/pageobjects/login.page.ts
@@ -0,0 +1,23 @@
+
+class LoginPage{
+ 
+    public get inputUsername () {
+        return $('#username');
+    }
+
+    public get inputPassword () {
+        return $('#password');
+    }
+
+    public get btnSubmit () {
+        return $('button[type="submit"]');
+    }
+
+    public async login (username: string, password: string) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+}
+
+export default new LoginPage();
